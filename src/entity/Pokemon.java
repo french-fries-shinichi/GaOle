b2367c4dc694c03fb6c707f91240d43b4cd4373f
Yuiikai <kaiye@localhost>
2025-07-16 07:13:39 +0800

    Second commit testing

--- src/entity/Pokemon.java
+++ src/entity/Pokemon.java
@@ -27,6 +27,7 @@ public class Pokemon {
 		this.speed = speed;
 	}
 	
+	//All Pokemon types
 	private static final String[] types = 
 		{
 				"Normal", "Fire", "Water", "Electric", "Ice" ,"Grass", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"
